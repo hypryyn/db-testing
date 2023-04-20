f0ce4db804e1ab76932fb53a862208d9b8f00636
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-04-20 22:21:11 +0700

    alternative to model.find()

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const person = new Person({
 
 // const fruit = new Fruit ({
 //     name: "Apple",
-//     raing: 7,
+//     rating: 7,
 //     review: "Pretty solid as a fruit."
 // });
 
@@ -40,7 +40,25 @@ const banana = new Fruit({
 
 // person.save();
 
-Fruit.insertMany([kiwi, orange, banana]);
+// Fruit.insertMany([kiwi, orange, banana]);
+//<<  use this to find all queries >>
+// Fruit.find()
+//     .then(function (fruits) {
+//         console.log(fruits);
+//     })
+//     .catch(function (err) {
+//         console.log(err);
+// });
+//<< use this to find specific object >>
+Fruit.find()
+    .then(function (fruits) {
+        fruits.forEach(function (fruit) {
+            console.log(fruit.name);
+        });
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
 
 // run()
 // async function run() {
